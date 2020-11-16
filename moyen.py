b57d7a5f14ad7603574b5762c6c13d7cb5ee57fe
jsjsjsjass <68822412+jsjsjsjass@users.noreply.github.com>
2020-11-16 12:30:54 +0200

    Add files via upload

---
+++ moyen.py
@@ -0,0 +1,15 @@
+x1 = int(input('choisi premier xi: '))
+y1 = int(input('choisi leur ni: '))
+x2 = int(input('choisi dexiem xi: '))
+y2 = int(input('choisi leur ni: '))
+x3 = int(input('choisi troisiem xi: '))
+y3 = int(input('choisi leur ni: '))
+x4 = int(input('choisi quatriem xi: '))
+y4 = int(input('choisi leur ni: '))
+ope = input('choisi 1: ')
+total = int(input('choisi total: '))
+if ope == '1':
+    print((x1 * y1 + x2 * y2 + x3 * y3 + x4 * y4)/total)
+else:
+    Faux
+    
