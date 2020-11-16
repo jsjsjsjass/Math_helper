6e763941fc081c2057b200e83532dba733275f8f
jsjsjsjass <68822412+jsjsjsjass@users.noreply.github.com>
2020-11-16 12:23:55 +0200

    Add files via upload

---
+++ calcul.py
@@ -0,0 +1,42 @@
+print('welcome to my calculaiter')
+
+
+
+
+
+hi = input('commen t,appel tu?: ')
+
+
+print('BONJOUR ET BIEN VENU '+hi)
+
+num1 = int(input('C)crire premier nombre: '))
+
+
+symbole = input('C)crire le symbole(+,*,- ou /): ')
+
+
+num2 = int(input('C)crire dexiem nombre: '))
+
+
+
+
+if symbole == '+':
+    print(num1 + num2)
+    
+if symbole == '-':
+    
+    print(num1 - num2)
+    
+    
+    
+    
+if symbole == '*':
+    print(num1 * num2)
+    
+    
+if symbole == '/':
+    print(num1 / num2)
+    
+      
+else:
+    print('NOT FOUND!!!')
